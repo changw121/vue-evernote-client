511170f0a3777e18a3a501f2a41d57348260d4df
changw <2283992824@qq.com>
2022-11-10 08:59:51 +0800

    新增sidebar组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <sidebar></sidebar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import sidebar from './components/Sidebar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {sidebar}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css";
+
+  * {
+    box-sizing: border-box;  /*你设置的宽度width=内容宽度+padding+border*/
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;  /*不让页面中所有的链接有下划线*/
+    color: #444;
+  }
+
+  ul, li {
+    list-style: none; /*删除列表中的原点*/
+  }
+
+  html, body, #app{
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;  /*设置字体*/
+    font-size: 14px;
+    background-color: #eee;
+  }
+
+
+  /*.btn {*/
+  /*  color: #666;*/
+  /*  font-size: 12px;*/
+  /*  padding: 2px 4px;*/
+  /*  background-color: #fff;*/
+  /*  border: none;*/
+  /*  cursor: pointer;*/
+  /*  display: inline-block;*/
+  /*}*/
+
+  #app {
+    /*将其子元素拉伸至行高*/
+    display: flex;
+    align-items: stretch;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
 import Notebooks from '@/components/NotebookList'
 import NoteDetail from '@/components/NoteDetail'
@@ -9,11 +8,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/login',
       component: Login
