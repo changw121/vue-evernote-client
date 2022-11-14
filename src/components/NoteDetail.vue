28218bb46d82b4e6190f1c9be5fa3b5788c5a62d
changw <2283992824@qq.com>
2022-11-14 10:47:59 +0800

    笔记详情列表的结构和侧边栏样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,16 +1,21 @@
 <template>
-  <div id="note-detail">
-<!--    如果你的路由是/note/:noteId则再params里面找到后边的参数-->
-<!--    <h1>{{msg}} : {{$route.params.noteId}}</h1>-->
-<!--    如果你的路由是/note?notebookId则在query里面找到后边的参数-->
-    <h1>notebookId: {{$route.query.notebookId}}</h1>
-    <h1>noteId: {{$route.query.noteId}}</h1>
+  <div id="note" class="etail">
+    <note-sidebar></note-sidebar>
+    <div class="note-detail">
+      <!--    如果你的路由是/note/:noteId则再params里面找到后边的参数-->
+      <!--    <h1>{{msg}} : {{$route.params.noteId}}</h1>-->
+      <!--    如果你的路由是/note?notebookId则在query里面找到后边的参数-->
+      <h1>notebookId: {{$route.query.notebookId}}</h1>
+      <h1>noteId: {{$route.query.noteId}}</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import Auth from '@/apis/auth'
+import NoteSidebar from '@/components/NoteSidebar'
 export default {
+  components: {NoteSidebar},
   data(){
     return {
       msg: '笔记详情页'
@@ -28,8 +33,11 @@ export default {
 
 </script>
 
-<style scoped>
-h1{
-  color: blue;
-}
+<style lang="less" scoped>
+  #note {
+    display: flex;
+    align-items: stretch;  //?
+    background-color: #fff;
+    flex: 1;  //占满剩下的空间
+  }
 </style>
